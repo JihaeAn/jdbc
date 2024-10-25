1c7bb73d9e9dfe043282b2e035ef868942dd1053
jihae <dkswlgo6615@naver.com>
2024-10-25 17:01:33 +0900

    예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,25 +1,37 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.net.ConnectException;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
-    void checked() {
+    void unchecked() {
         Controller controller = new Controller();
         assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
-        public void request() throws SQLException, ConnectException {
+        public void request() {
             service.logic();
         }
     }
@@ -28,21 +40,43 @@ public class UnCheckedAppTest {
         Repository repository = new Repository();
         NetworkClient networkClient = new NetworkClient();
 
-        public void logic() throws SQLException, ConnectException {
+        public void logic() {
             repository.call();
             networkClient.call();
         }
     }
 
     static class NetworkClient {
-        public void call() throws ConnectException {
-            throw new ConnectException("연결 실패");
+        public void call() {
+            throw new RuntimeConnectException("연결 실패");
         }
     }
 
     static class Repository {
-        public void call() throws SQLException {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
             throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException() {
+        }
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
         }
     }
 }
